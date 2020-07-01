965716a4a72b4f3e218928cabfe26a48e29a7943
pat-s <patrick.schratz@gmail.com>
2020-07-01 22:04:37 +0200

    fix org name

--- tic.R
+++ tic.R
@@ -1,5 +1,5 @@
 get_stage("install") %>%
-  add_step(step_install_github("dsbbfindx/FIND_COV_19_Tracker@r-package"))
+  add_step(step_install_github("dsbbfinddx/FIND_COV_19_Tracker@r-package"))
 
 get_stage("before_deploy") %>%
   add_step(step_setup_push_deploy())
