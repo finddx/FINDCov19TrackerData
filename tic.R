39728b22a083cbef6cd525a40c149f85494ff42a
pat-s <patrick.schratz@gmail.com>
2020-08-08 09:22:37 +0200

    ups

--- tic.R
+++ tic.R
@@ -3,7 +3,7 @@ get_stage("before_deploy") %>%
   add_step(step_setup_push_deploy())
 
 get_stage("deploy") %>%
-  add_step(step_install_github("dsbbfinddx/FINDCov19Tracker"))
+  add_step(step_install_github("dsbbfinddx/FINDCov19Tracker")) %>%
   add_code_step(FindCovTracker::process_jhu_data(), prepare_call = TRUE) %>%
   add_code_step(FindCovTracker::process_test_data(), prepare_call = TRUE) %>%
   add_code_step(FindCovTracker::create_shiny_data(), prepare_call = TRUE) %>%
