de864cb47ac38c21baa2fca63942eb0e09812ecf
pat-s <patrick.schratz@gmail.com>
2020-08-08 09:15:29 +0200

    install FindCovTracker19 in deploy stage

--- tic.R
+++ tic.R
@@ -1,11 +1,9 @@
-get_stage("install") %>%
-  add_step(step_install_github("dsbbfinddx/FINDCov19Tracker"))
-
 get_stage("before_deploy") %>%
   add_step(step_setup_ssh()) %>%
   add_step(step_setup_push_deploy())
 
 get_stage("deploy") %>%
+  add_step(step_install_github("dsbbfinddx/FINDCov19Tracker"))
   add_code_step(FindCovTracker::process_jhu_data(), prepare_call = TRUE) %>%
   add_code_step(FindCovTracker::process_test_data(), prepare_call = TRUE) %>%
   add_code_step(FindCovTracker::create_shiny_data(), prepare_call = TRUE) %>%
