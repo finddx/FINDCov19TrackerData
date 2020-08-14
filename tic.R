985e562c8e9a37aadc9c3536548196ef39f303d2
pat-s <patrick.schratz@gmail.com>
2020-08-14 15:52:43 +0200

    update package name

--- tic.R
+++ tic.R
@@ -4,9 +4,9 @@ get_stage("before_deploy") %>%
 
 get_stage("deploy") %>%
   add_step(step_install_github("dsbbfinddx/FINDCov19Tracker")) %>%
-  add_code_step(FindCovTracker::process_jhu_data(), prepare_call = TRUE) %>%
-  add_code_step(FindCovTracker::process_test_data(), prepare_call = TRUE) %>%
-  add_code_step(FindCovTracker::create_shiny_data(), prepare_call = TRUE) %>%
+  add_code_step(FINDCov19Tracker::process_jhu_data(), prepare_call = TRUE) %>%
+  add_code_step(FINDCov19Tracker::process_test_data(), prepare_call = TRUE) %>%
+  add_code_step(FINDCov19Tracker::create_shiny_data(), prepare_call = TRUE) %>%
   add_step(step_do_push_deploy(commit_paths = c(
     "processed/coronavirus_cases.csv",
     "processed/coronavirus_tests.csv",
