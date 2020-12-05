15d02cc412be1606f4a35c8129e874fc79395310
Yurii Zolotko <yurii.zolotko@pm.me>
2020-12-05 18:27:13 +0700

    Add docs

--- manual/run.py
+++ manual/run.py
@@ -12,10 +12,13 @@ def run_one_country(country):
 
 
 if __name__ == '__main__':
+    # collect list of all known countries
+    # by enumerating all attributes of TestDefaultSuite starting with "test_"
     countries = []
     for m in dir(TestDefaultSuite):
         if m.startswith('test_'):
             countries.append(m[5:])
+    # parse command line arguments
     parser = argparse.ArgumentParser(
         description='Run all or some tests.',
         epilog="If countries list omitted, will run tests for all countries.")
@@ -26,14 +29,16 @@ if __name__ == '__main__':
                         help='Country name')
     country_list = []
     args = parser.parse_args()
+    # run test(s)
     if args.countries:
-        # check specified country names are valid
+        # run only countries specified in command line
         for country in args.countries:
             country_list.append(run_one_country(country))
     else:
         # run all countries in parallel
         with concurrent.futures.ThreadPoolExecutor() as executor:
             country_list = executor.map(run_one_country, countries)
+    # save results
     with open("list_data.txt", 'w', encoding="utf-8") as f:
         for item in country_list:
             f.write("%s\n" % item)
