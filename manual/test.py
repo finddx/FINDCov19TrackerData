581def6135919f60055438a382615d68210ef656
angelicambg <angelicambg@gmail.com>
2020-12-16 22:45:53 +0100

    adding arcgis country (Egypt)

--- manual/test.py
+++ manual/test.py
@@ -340,3 +340,10 @@ class TestDefaultSuite(unittest.TestCase):
     self.vars["tests"] = self.driver.find_element(By.ID, "value-item-virus_tests_conducted-total-cumvirustests-1_modal").text
     # 4 | close |  | 
     self.driver.close()
+    
+  def test_egypt(self):
+    self.driver.get("https://africacdc.maps.arcgis.com/apps/opsdashboard/index.html#/9d8d4add4dcb456997fd83607b5d0c7c")
+    self.driver.find_element(By.CSS_SELECTOR, "circle:nth-child(3)").click()
+    self.vars["tests"] = self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(8) .esriNumericValue").text
+    self.driver.close()
+    
