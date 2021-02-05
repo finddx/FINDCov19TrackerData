0c24415f7e4059409a7b9d6d6ac2dd94b67adbcd
pat-s <patrick.schratz@gmail.com>
2021-02-05 14:22:14 +0100

    enfore dplyr 1.0.3 for now

--- tic.R
+++ tic.R
@@ -4,6 +4,8 @@ get_stage("before_deploy") %>%
 
 get_stage("deploy") %>%
   add_step(step_install_github("dsbbfinddx/FINDCov19Tracker")) %>%
+  # FIXME https://github.com/dsbbfinddx/FINDCov19TrackerData/issues/30
+  add_code_step(remotes::install_version("dplyr", "1.0.3")) %>%
   add_code_step(FINDCov19Tracker::create_shiny_data(), prepare_call = TRUE) %>%
   add_step(step_do_push_deploy(commit_paths = c(
     "processed/coronavirus_cases.csv",
