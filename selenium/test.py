3fc62c2f0dcdf5e0f73c1569c7ec4a28988939af
angelicambg <38508957+angelicambg@users.noreply.github.com>
2021-06-16 14:21:52 +0200

    Fix Fiji

--- selenium/test.py
+++ selenium/test.py
@@ -436,8 +436,10 @@ class TestDefaultSuite(unittest.TestCase):
     self.driver.get("http://www.health.gov.fj/")
     url=self.driver.find_element(By.XPATH, "(//a[contains(text(),\'COVID-19 Update\')])[3]").get_attribute('href')
     self.driver.get(url)
-    self.vars["tests_cumulative"] = self.driver.find_element(By.XPATH, "//p[contains(.,\'laboratory tests had been conducted\')]").text
-    self.vars["tests_cumulative"] = self.vars["tests_cumulative"].split('total of')[1].split('lab')[0]
+    self.vars["tests_cumulative"] = self.driver.find_element(By.XPATH, "//p[contains(.,\'tested since testing began in early\')]").text
+    self.vars["tests_cumulative"] = self.vars["tests_cumulative"].split('with')[1].split('tested')[0]
+    print("Fiji")
+    print(self.vars)
     self.driver.close()
 
   def test_finland(self):
