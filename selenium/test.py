828cd4a12f0a18107554a31b66c3c604fd2a3e35
Adriana <ac19j080@campus.unibe.ch>
2022-03-15 17:59:54 +0100

    fix selenium Cambodia

--- selenium/test.py
+++ selenium/test.py
@@ -318,9 +318,9 @@ class TestDefaultSuite(unittest.TestCase):
     # self.vars["date"] =date.today().strftime("%Y-%m-%d")
     self.driver.get("http://cdcmoh.gov.kh")
     self.driver.set_window_size(1542, 830)
-    self.driver.find_element(By.CSS_SELECTOR, "#content > div.blog > div > div > div.item.column-1 > div:nth-child(9) > strong").click()
-    self.vars["tests_cumulative"] = self.driver.find_element(By.CSS_SELECTOR, "#content > div.blog > div > div > div.item.column-1 > div:nth-child(9) > strong").text
-    self.vars["tests_cumulative"] = self.vars["tests_cumulative"].split(' ')[1]
+    self.driver.find_element(By.CSS_SELECTOR, "#content > div.blog > div.items-row.cols-1.row-1.row-fluid.clearfix > div > div > div:nth-child(6) > span:nth-child(1) > strong:nth-child(1)").click()
+    self.vars["tests_cumulative"] = self.driver.find_element(By.CSS_SELECTOR, "#content > div.blog > div.items-row.cols-1.row-1.row-fluid.clearfix > div > div > div:nth-child(6) > span:nth-child(1) > strong:nth-child(1)").text
+    #self.vars["tests_cumulative"] = self.vars["tests_cumulative"].split(' ')[1]
     print("Cambodia")
     print(self.vars)
     self.driver.close()
