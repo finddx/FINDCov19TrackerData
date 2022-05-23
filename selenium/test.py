84b236bc38c36301efdb1cb0dee112a32b305ef1
Adriana <ac19j080@campus.unibe.ch>
2022-05-23 18:34:51 +0200

    Selenium: update Peru link [skip ci]

--- selenium/test.py
+++ selenium/test.py
@@ -924,7 +924,7 @@ class TestDefaultSuite(unittest.TestCase):
   def test_peru(self):
     # self.vars["date"] =date.today().strftime("%Y-%m-%d")
     self.driver.set_window_size(1333, 813)
-    self.driver.get("https://app.powerbi.com/view?r=eyJrIjoiOWIzMjZhZDMtZDlkMi00NjhjLWJhMGYtMTlkMzFmMzQ4NTc3IiwidCI6IjM0MGJjMDE2LWM2YTYtNDI2Ni05NGVjLWE3NDY0YmY5ZWM3MCIsImMiOjR9")
+    self.driver.get("https://app.powerbi.com/view?r=eyJrIjoiY2JjODMwMDktMjExNy00NDdjLTg0MjItYzYxNzZiMzZiZjcyIiwidCI6IjM0MGJjMDE2LWM2YTYtNDI2Ni05NGVjLWE3NDY0YmY5ZWM3MCIsImMiOjR9")
     self.driver.set_page_load_timeout(40)
     self.driver.implicitly_wait(40)
     self.driver.execute_script("window.scrollTo(0,0)")
